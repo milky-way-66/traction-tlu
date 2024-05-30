8bea504929ad4976171d3fb81c410b8afa1877bc
longnv <longnv@kaopiz.com>
2024-05-30 21:25:35 +0700

    add vietnamese translate

--- services/tenant-ui/frontend/src/plugins/i18n/i18n.ts
+++ services/tenant-ui/frontend/src/plugins/i18n/i18n.ts
@@ -2,7 +2,7 @@ import { createI18n } from 'vue-i18n';
 import messages from '@intlify/unplugin-vue-i18n/messages';
 
 export default createI18n({
-  locale: 'en',
+  locale: 'vi',
   fallbackLocale: 'en',
   messages,
 });
